fabd76d9358c2b68ba4c50679aee928c7e403d32
Orange <dasoncheng@foxmail.com>
2016-04-07 16:16:20 +0800

    Handlebars

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
 var router = require('./routers/main');
+var exphbs = require('express-handlebars');
 var app = express();
 
-app.set('port',process.env.PORT||3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views',__dirname+'/views');
-app.set('view engine','jade');
+
+// app.set('view engine','jade');
+app.engine('html', exphbs({
+    extname: '.html'
+}));
+app.set('view engine', 'html');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/',router);
+app.use('/', router);
 
-app.listen(app.get('port'),function(){
-  console.log('Exprese started on http://localhost/:'+app.get('port')+';press Ctrl+C to terminate!');
-})
+app.listen(app.get('port'), function() {
+    console.log('Exprese started on http://localhost/:' + app.get('port') + ';press Ctrl+C to terminate!');
+});
